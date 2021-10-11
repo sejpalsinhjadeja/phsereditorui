ce0d0d512b90b748b6150a8bdd923da414315feb
Ajju <ajinkya.g@yudiz.com>
2021-10-11 13:21:12 +0530

    Auto stash before merge of "main" and "origin/main"

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -65,29 +65,74 @@ class Level extends Phaser.Scene {
 		header_bg_img.setOrigin(0.5, 0);
 		header.add(header_bg_img);
 
-		// Profile
-		const profile = this.add.container(140, 70);
-		header.add(profile);
+		// defaultProfile
+		const defaultProfile = this.add.image(142, 70, "defaultProfile");
+		defaultProfile.scaleX = 0.7;
+		defaultProfile.scaleY = 0.7;
+		header.add(defaultProfile);
 
 		// white_border
-		const white_border = this.add.image(0, 0, "White-border");
+		const white_border = this.add.image(143, 70, "White-border");
 		white_border.scaleX = 0.7;
 		white_border.scaleY = 0.7;
-		profile.add(white_border);
+		header.add(white_border);
 
-		// default_profile
-		const default_profile = this.add.image(0, 0, "defaultProfile");
-		default_profile.scaleX = 0.7;
-		default_profile.scaleY = 0.7;
-		profile.add(default_profile);
+		// cash_icon
+		const cash_icon = this.add.image(490, 76, "Cash-icon");
+		cash_icon.scaleX = 0.4;
+		cash_icon.scaleY = 0.4;
+		header.add(cash_icon);
 
-		this.header = header;
+		// tH02_Yellow_Box_Ui
+		const tH02_Yellow_Box_Ui = this.add.image(1805, 70, "TH02_Yellow-Box-Ui");
+		tH02_Yellow_Box_Ui.scaleX = 2;
+		tH02_Yellow_Box_Ui.scaleY = 2;
+		header.add(tH02_Yellow_Box_Ui);
+
+		// tH02_Yellow_Box_Ui_1
+		const tH02_Yellow_Box_Ui_1 = this.add.image(1659, 69, "TH02_Yellow-Box-Ui");
+		tH02_Yellow_Box_Ui_1.scaleX = 2;
+		tH02_Yellow_Box_Ui_1.scaleY = 2;
+		header.add(tH02_Yellow_Box_Ui_1);
+
+		// menu_icon
+		const menu_icon = this.add.image(1656, 69, "Menu-icon");
+		menu_icon.scaleX = 0.2;
+		menu_icon.scaleY = 0.2;
+		header.add(menu_icon);
+
+		// reload
+		const reload = this.add.image(1803, 67, "reload");
+		reload.scaleX = 0.3;
+		reload.scaleY = 0.3;
+		header.add(reload);
+
+		// playerNametxt
+		const playerNametxt = this.add.text(224, 60, "", {});
+		playerNametxt.text = "playername";
+		playerNametxt.setStyle({"fontSize":"20px","fontStyle":"italic"});
+		header.add(playerNametxt);
+
+		// balancetxt
+		const balancetxt = this.add.text(541, 69, "", {});
+		balancetxt.text = "128.49";
+		balancetxt.setStyle({"fontSize":"20px"});
+		header.add(balancetxt);
+
+		// oTableId
+		const oTableId = this.add.text(730, 72, "", {});
+		oTableId.text = "Table Id:#46274f1a";
+		oTableId.setStyle({"fontSize":"20px"});
+		header.add(oTableId);
+
+		// oDecktext
+		const oDecktext = this.add.text(967, 71, "", {});
+		oDecktext.text = "Point Rummy- 2 Deck ";
+		oDecktext.setStyle({"fontSize":"20px"});
+		header.add(oDecktext);
 
 		this.events.emit("scene-awake");
 	}
-
-	/** @type {Phaser.GameObjects.Container} */
-	header;
 
 	/* START-USER-CODE */
 
